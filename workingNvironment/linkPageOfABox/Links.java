e1aad254ad80a9d5e2a63888b7f5d19a1e5f5f93
Ezgi Lena Sönmez <lena.sonmez@ug.bilkent.edu.tr>
2020-05-19 02:39:48 +0300

    for the link page

---
+++ workingNvironment/linkPageOfABox/Links.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkpageofabox;
+
+/**
+ *
+ * @author EZGİ LENA SÖNMEZ
+ */
+public class Links
+{
+    private String links;
+    
+    public Links()
+    {
+        this.links = "";
+    }
+    
+    public Links( String links )
+    {
+        this.links = links;
+    }
+    
+    public String getLinks()
+    {
+        return links;
+    }
+    public void setLinks( String links )
+    {
+        this.links = links;
+    }    
+}
